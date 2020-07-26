2bee5e6d8981867ccd6b0d00a9b1e67cad36bb37
Tanuka-Mondal <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-07-26 18:18:30 +0530

    Update input.py

--- input.py
+++ input.py
@@ -1,2 +1,4 @@
+#Write a program that uses input to prompt a user for their name and then welcomes them. Note that input will pop up a dialog box.
+
 name = input("Enter your name")
 print("Hello "+name)
